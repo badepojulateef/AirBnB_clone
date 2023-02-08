5899adbeaf6fb163739d7c5c73bf2a559b4ddc1d
Adepoju Lateef <badepojulateef@gmail.com>
2023-02-08 11:50:37 +0100

    added __str__ representation

--- models/base_model.py
+++ models/base_model.py
@@ -10,8 +10,20 @@ class BaseModel:
     A base or super class from which all other classes
     inherits the common attributes/methods
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Initializes the instance attributes """
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if not kwargs:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ("created_at", "updated_at"):
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
+
+    def __str__(self):
+        """ Returns a new and customized string represented """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
