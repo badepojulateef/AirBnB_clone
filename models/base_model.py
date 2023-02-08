c5dd8a4ea98495f79f8111ecda983da4cdb9e746
Adepoju Lateef <badepojulateef@gmail.com>
2023-02-08 11:56:17 +0100

    line 29 broken into two because it's too long

--- models/base_model.py
+++ models/base_model.py
@@ -26,4 +26,5 @@ class BaseModel:
 
     def __str__(self):
         """ Returns a new and customized string represented """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, self.__dict__)
